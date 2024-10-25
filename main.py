2f2ca5850d3f3024dcb1e350ce12ae8ad0c382ce
mountainduu <danieldusd@gmail.com>
2024-10-25 01:42:39 +0100

    middle of making code nice + 2x2 binning

--- main.py
+++ main.py
@@ -3,48 +3,110 @@ import time
 import asyncio
 from PIL import Image
 import numpy as np
+import argparse
 
 
 
 async def take_picture(cam):
+    """Async take a picture and returns it as an array
+    cam -- The picam2 object
+    """
     return cam.capture_array("main")
 
 async def save_to_file(image_list, filename):
-    # Convert the numpy array to a PIL Image object
-    image_array = np.array(image_list, dtype=np.uint8)
-    image = Image.fromarray(image_array)
-    
-    # Save the image as a JPG file
-    image.save(f"{filename}.jpg", "JPEG")
-    print(f"Image saved to {filename}.jpg")
+    """Does nothing for now
+    Later might be changed to send data over wifi or other interface
+    """
 
+    return
 
-async def main():
-
+def initialize_camera():
+    """Initializes the camera and returns a picam2 object ready to be configured
+    """
     picam2 = Picamera2()
-    camera_config = picam2.create_still_configuration({"size": (4056, 3040)}, buffer_count=1, queue=True, controls = {'NoiseReductionMode': 0})
-    picam2.configure(camera_config)
-    check = picam2.camera_configuration()['raw']
-    print(camera_config)
-    print(picam2.camera_controls)
+    return picam2
+    
+def print_sensor_modes():
+    """Prints the modes of the camera
+    """
+    modes = picam2.sensor_modes
+    print(modes)
 
 
-    picam2.start_preview(Preview.NULL)
+def select_2x2_binning(picam2):
+    """Configures the settigns to use 2x2 binning mode and returns it
+    """
+
+    modes = picam2.sensor_modes
+    mode = modes[2]
+    camera_config = picam2.create_still_configuration(
+            {"size": mode['size']}, 
+            buffer_count=4,
+            raw={'format': mode['unpacked']}, 
+            sensor={'output_size': mode['size'],
+                    'bit_depth': mode['bit_depth']})
+
+    return camera_config
+
+def select_full_res(picam2):
+    """Configures the camera to take full res photos
+    returns the settings
+    """
+
+    camera_config = picam2.create_still_configuration(
+            {"size": (4056, 3040)},
+            buffer_count=1,
+            queue=False,
+            controls = {'NoiseReductionMode': 0})
+
+    return camera_config
+async def main():
+    """Main function
+    kwargs -- any input parameters 
+
+    binning -- boolean, if true, 2x2 binning, false, full re 
+    write_to_sd -- boolean, if true: write to SD, if false: don't
+    """
+
+    '''
+    args = parser.parge_args()
+    binning = args.binning
+    write_to_sd = args.write_to_sd
+
+
+    '''
+    picam2 = initialize_camera()
+    binning = True
+    if (binning == True):
+        camera_config = select_2x2_binning(picam2)
+        picam2.configure(camera_config)
+    
+    else:
+        camera_config = select_full_res(picam2)
+
+        picam2.configure(camera_config)
+
+
+    
+
+
     picam2.start()
 
     current_time = time.time()
     intergral = 0
 
     image_array=[[0,0,0]]
-    for i in range(10):
+
+    for i in range(20):
         temp_current_time = time.time()
         print(temp_current_time - current_time)
         intergral += temp_current_time - current_time
         current_time = temp_current_time
+        image_array, _ = await asyncio.gather(take_picture(picam2), save_to_file(image_array, f'{i}.txt'))
 
-        #image_array = await take_picture(picam2)
-        #await save_to_file(image_array, f'captured_image_{i}.npy')
-        iamge_array, _ = await asyncio.gather(take_picture(picam2), save_to_file(image_array, f'captured_image_{i}'))
+        #picam2.capture_file(f"{i}.jpg")
+    print(intergral) 
+
 
 # Run the main function
 if __name__ == "__main__":
