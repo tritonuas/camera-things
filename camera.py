d6522c711ab9b1782e0a926bdd84d6d2a1935c75
mountainduu <danieldusd@gmail.com>
2024-10-31 22:53:14 +0000

    refactoring done, camera is now object

--- camera.py
+++ camera.py
@@ -9,7 +9,9 @@ import argparse
 
 class Camera:
 
-    def __init__(**kwargs):
+
+
+    def __init__(self, **kwargs):
         
 
         """Main function
@@ -20,8 +22,6 @@ class Camera:
         time -- if enabled, log the fps in a seperate csv
         location -- sets the location to save to file
         file-format -- sets the file format (jpg, png, dng)
-        number -- sets the number of pictures to take
-                  1 default, 0 -> infinite
         """
 
         VALID_FILE_TYPES=[
@@ -37,123 +37,45 @@ class Camera:
 
         self.binning = kwargs.get('binning', False)
         self.save = kwargs.get('save', False)
-        self.time = kwargs.get('logging', False)
+        self.time = kwargs.get('time', False)
         self.verbose = kwargs.get('verbose', False)
 
         #Handles the location settings
-        self.location = kwargs.get('location', '')
+        self.location = kwargs.get('location', '.')
+        if (self.location ==''):
+            self.location = '.'
+
         if (self.location[-1] != '/'):
             self.location += '/'
 
-
-        #handles the iteration stuff, default is 1
-        self.iterations = kwargs.get('iterations', 1)
-
-
-    def initialize_and_configure():
-
-        #Initializes the camera
-        self.picam2 = initialize_camera()
-        #Set the appropirate camera config
-        if (self.binning == True):
-            self.picam2.configure(select_2x2_binning(picam2))
-        else:
-            self.picam2.configure(select_full_res(picam2))
-
-
-    def start():
-        #Start the camera
-        self.picam2.start()
-
-
-    def take_photo():
-
-        #Here incase photos written to array
-        self.image_array=[[0,0,0]]
-
-        #start the time logging stuff if enabled
-        if (self.time):
-            self.timer = timeLogging()
-            self.timer.start_timer()
-
-        # Determine the number of leading zeros
-        # depending on the number of iterations
-        if (self.save):
-            self.digits = len(str(self.iterations))
-        if (self.iterations == 0):
-            self.digits = 8
-
-        try:
-            self.i = 0
-            while True:
-                #Take the photo and maybe save it
-                if (self.save):
-                    capture_file(self.picam2, 
-                                 format_number(self.i, self.digits),
-                                 self.file_format,
-                                 self.location) 
-
-                else:
-                    """
-                    self.image_array, _ = await asyncio.gather(
-                            take_picture(self.picam2),
-                            save_to_file(self.image_array, f'{format_number(i, digits)}.txt'))
-                    """
-                    # Remove later, right now, we only need to be saving
-                    break
-
-                #If logging is enabled then log it, and if verbose, then print
-                if (self.time_logging):
-                    if (self.verbose):
-                        self.timer.log_and_print()
-                    else:
-                        self.timer.log()
-
-                #Count the iterations
-                self.i += 1
-                if (self.i == self.iterations):
-                    break
-        #TODO: Make it so then when the method is ran in main file, this still
-        #      Gets the KeyboardInterrupt error
-        #If KeyboardInterrupt to break the while True, time data is still saved
-        except KeyboardInterrupt:
-            if (self.time_logging):
-                self.timer.save_to_file(self.location)
-                print("saved to file")
-            return
-        if (self.time_logging):
-            self.timer.save_to_file(self.location)
-            print("saved to file")
-
-
-    async def take_picture(cam):
+    async def take_picture(self, cam):
         """Async take a picture and returns it as an array
         cam -- The picam2 object
         """
         return cam.capture_array("main")
 
-    async def save_to_file(image_list, filename):
+    async def save_to_file(self, image_list, filename):
         """Does nothing for now
         Later might be changed to send data over wifi or other interface
         """
 
         return
 
-    def initialize_camera():
+    def initialize_camera(self):
         """Initializes the camera and returns a 
         picam2 object ready to be configured
         """
         picam2 = Picamera2()
         return picam2
         
-    def print_sensor_modes():
+    def print_sensor_modes(self):
         """Prints the modes of the camera
         """
         modes = picam2.sensor_modes
         print(modes)
 
 
-    def select_2x2_binning(picam2):
+    def select_2x2_binning(self, picam2):
         """Configures the settigns to use 2x2 binning mode and returns the config
         """
 
@@ -168,7 +90,7 @@ class Camera:
 
         return camera_config
 
-    def select_full_res(picam2):
+    def select_full_res(self, picam2):
         """Configures the camera to take full res photos
         returns the settings
         """
@@ -181,15 +103,99 @@ class Camera:
 
         return camera_config
 
-    def capture_file(picam2, iteration, file_format, location):
+    def capture_file(self, picam2, iteration, file_format, location):
         if (file_format == 'dng'):
             picam2.capture_file(f"{location}{iteration}.dng", 'raw')
 
         else:
             picam2.capture_file(f"{location}{iteration}.{file_format}")
 
-    def format_number(num, digits):
+    def format_number(self, num, digits):
         return f"{num:0{digits}d}"
+
+
+
+
+
+    def initialize_and_configure(self):
+
+        #Initializes the camera
+        self.picam2 = self.initialize_camera()
+        #Set the appropirate camera config
+        if (self.binning == True):
+            self.picam2.configure(self.select_2x2_binning(self.picam2))
+        else:
+            self.picam2.configure(self.select_full_res(self.picam2))
+
+
+    def start(self):
+        #Start the camera
+        self.picam2.start()
+
+
+    def take_photo(self):
+        self.take_photos(1)
+    def take_photos(self, iterations):
+
+        #Here incase photos written to array
+        self.image_array=[[0,0,0]]
+
+        #start the time logging stuff if enabled
+        if (self.time):
+            self.timer = timeLogging()
+            self.timer.start_timer()
+
+        # Determine the number of leading zeros
+        # depending on the number of iterations
+        if (self.save):
+            self.digits = len(str(iterations))
+        if (iterations == 0):
+            self.digits = 8
+
+        print(iterations)
+        try:
+            i = 0
+            while True:
+                #Take the photo and maybe save it
+                if (self.save):
+                    self.capture_file(self.picam2, 
+                                 self.format_number(i, self.digits),
+                                 self.file_format,
+                                 self.location) 
+
+                else:
+                    """
+                    self.image_array, _ = await asyncio.gather(
+                            take_picture(self.picam2),
+                            save_to_file(self.image_array, f'{format_number(i, digits)}.txt'))
+                    """
+                    # Remove later, right now, we only need to be saving
+                    break
+
+                #If logging is enabled then log it, and if verbose, then print
+                if (self.time):
+                    if (self.verbose):
+                        self.timer.log_and_print()
+                    else:
+                        self.timer.log()
+
+                #Count the iterations
+                i += 1
+                if (i == iterations):
+                    break
+        #TODO: Make it so then when the method is ran in main file, this still
+        #      Gets the KeyboardInterrupt error
+        #If KeyboardInterrupt to break the while True, time data is still saved
+        except KeyboardInterrupt:
+            if (self.time):
+                self.timer.save_to_file(self.location)
+                print("saved to file")
+            return
+        if (self.time):
+            self.timer.save_to_file(self.location)
+            print("saved to file")
+
+
 
 
                 
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from camera import Camera
 from picamera2 import Picamera2, Preview
 from time_logging import timeLogging
 import time
@@ -13,70 +14,6 @@ VALID_FILE_TYPES=[
         'dng']
 
 
-async def take_picture(cam):
-    """Async take a picture and returns it as an array
-    cam -- The picam2 object
-    """
-    return cam.capture_array("main")
-
-async def save_to_file(image_list, filename):
-    """Does nothing for now
-    Later might be changed to send data over wifi or other interface
-    """
-
-    return
-
-def initialize_camera():
-    """Initializes the camera and returns a 
-    picam2 object ready to be configured
-    """
-    picam2 = Picamera2()
-    return picam2
-    
-def print_sensor_modes():
-    """Prints the modes of the camera
-    """
-    modes = picam2.sensor_modes
-    print(modes)
-
-
-def select_2x2_binning(picam2):
-    """Configures the settigns to use 2x2 binning mode and returns the config
-    """
-
-    modes = picam2.sensor_modes
-    mode = modes[2]
-    camera_config = picam2.create_still_configuration(
-            {"size": mode['size']}, 
-            buffer_count=4,
-            raw={'format': mode['unpacked']}, 
-            sensor={'output_size': mode['size'],
-                    'bit_depth': mode['bit_depth']})
-
-    return camera_config
-
-def select_full_res(picam2):
-    """Configures the camera to take full res photos
-    returns the settings
-    """
-
-    camera_config = picam2.create_still_configuration(
-            {"size": (4056, 3040)},
-            buffer_count=1,
-            queue=False,
-            controls = {'NoiseReductionMode': 0})
-
-    return camera_config
-
-def capture_file(picam2, iteration, file_format, location):
-    if (file_format == 'dng'):
-        picam2.capture_file(f"{location}{iteration}.dng", 'raw')
-
-    else:
-        picam2.capture_file(f"{location}{iteration}.{file_format}")
-
-def format_number(num, digits):
-    return f"{num:0{digits}d}"
 
 
 async def main():
@@ -149,9 +86,6 @@ async def main():
     location = ''
     if (args.location != None):
         save = True
-        location = args.location
-        if (location[-1] != '/'):
-            location += '/'
 
 
     #handles the iteration stuff, default is 1
@@ -159,68 +93,26 @@ async def main():
     if (args.number != None):
         iterations = int(args.number)
 
-    #Initializes the camera
-    picam2 = initialize_camera()
+    rpicam = Camera(
+            binning=binning,
+            save=save,
+            time=time_logging,
+            location=location,
+            file_format=file_format,
+            verbose=verbose)
 
 
-    #Set the appropirate camera config
-    if (binning == True):
-        camera_config = select_2x2_binning(picam2)
-        picam2.configure(camera_config)
-    else:
-        camera_config = select_full_res(picam2)
-        picam2.configure(camera_config)
-
+    rpicam.initialize_and_configure()
 
     #Start the camera
-    picam2.start()
+    rpicam.start()
 
-    #Here incase photos written to array
-    image_array=[[0,0,0]]
+    print(iterations)
+    rpicam.take_photos(iterations)
 
-    #start the time logging stuff if enabled
-    if (time_logging):
-        timer = timeLogging()
-        timer.start_timer()
 
-    if (save):
-        digits = len(str(iterations))
-        print(digits)
-    if (iterations == 0):
-        digits = 8
 
-    try:
-        i = 0
-        while True:
-            #Take the photo and maybe save it
-            if (save):
-                capture_file(picam2, format_number(i, digits), file_format, location) 
 
-            else:
-
-                image_array, _ = await asyncio.gather(
-                        take_picture(picam2),
-                        save_to_file(image_array, f'{format_number(i, digits)}.txt'))
-            #If logging is enabled then log it, and if verbose, then print
-            if (time_logging):
-                if (verbose):
-                    timer.log_and_print()
-                else:
-                    timer.log()
-
-            #Count the iterations
-            i += 1
-            if (i == iterations):
-                break
-    #If KeyboardInterrupt to break the while True, time data is still saved
-    except KeyboardInterrupt:
-        if (time_logging):
-            timer.save_to_file(location)
-            print("saved to file")
-        return
-    if (time_logging):
-        timer.save_to_file(location)
-        print("saved to file")
 
             
 
