77a5a65f59ffbb6de89fc47a72e41d41956ef173
mountainduu <danieldusd@gmail.com>
2025-01-25 21:52:35 +0000

    ctrl c ctrl v

---
+++ CMakeFiles/simple-cam.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/simple-cam.dir/event_loop.cpp.o"
+  "CMakeFiles/simple-cam.dir/event_loop.cpp.o.d"
+  "CMakeFiles/simple-cam.dir/simple-cam.cpp.o"
+  "CMakeFiles/simple-cam.dir/simple-cam.cpp.o.d"
+  "simple-cam"
+  "simple-cam.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simple-cam.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
