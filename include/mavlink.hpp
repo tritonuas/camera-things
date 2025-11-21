257fec1123bb10245fe1f6bfa35dafa3ff685712
miyatakazuya <kazuyamiyata18@gmail.com>
2025-11-21 02:23:44 -0800

    fix: inlined duplicate variables

--- include/mavlink.hpp
+++ include/mavlink.hpp
@@ -10,27 +10,21 @@
 #include <future>
 #include <memory>
 #include <thread>
-
-
 #include <signal.h>
 #include <time.h>
 #include <sys/time.h>
-#include <pthread.h> // This uses POSIX Threads
-#include <unistd.h>  // UNIX standard function definitions
+#include <pthread.h>
+#include <unistd.h>
 #include <mutex>
-
 #include <common/mavlink.h>
 
 #include "port.hpp"
 #include "function_queue.hpp"
 
-
 namespace Mavlink {
-
 
     struct attitude_message {
         uint32_t time_boot_ms;
-
         float roll;
         float pitch;
         float yaw;
@@ -41,7 +35,6 @@ namespace Mavlink {
 
     struct gps_message {
         uint32_t time_boot_ms;
-
         int32_t lat;
         int32_t lon;
         int32_t alt;
@@ -52,105 +45,54 @@ namespace Mavlink {
         int16_t hdg;
     };
 
-
-
     struct Mavlink_Messages {
-
         int sysid;
         int compid;
-
-        // Heartbeat
         mavlink_heartbeat_t heartbeat;
-
-        // System Status
         mavlink_sys_status_t sys_status;
-
-        // Radio Status
         mavlink_radio_status_t radio_status;
-
-        // Local Position
         mavlink_local_position_ned_t local_position_ned;
-
-        // Global Position
         mavlink_global_position_int_t global_position_int;
-
-        // Local Position Target
         mavlink_position_target_local_ned_t position_target_local_ned;
-
-        // Global Position Target
         mavlink_position_target_global_int_t position_target_global_int;
-
-        // HiRes IMU
         mavlink_highres_imu_t highres_imu;
-
-        // Attitude
         mavlink_attitude_t attitude;
-
-        // System Parameters?
     };
 
-
     void start_read_thread();
 
+    inline Port *port;
+    inline bool time_to_exit;
+    inline int result;
+    inline pthread_t read_tid;
 
+    inline char reading_status;
+    inline char writing_status;
+    inline char control_status;
 
-    Port *port;
+    inline int system_id;
+    inline int autopilot_id;
+    inline int companion_id;
 
-    bool time_to_exit;
+    inline uint64_t write_count;
 
-    int result;
-    pthread_t read_tid;
+    inline Mavlink_Messages current_messages;
 
-    char reading_status;
-    char writing_status;
-    char control_status;
-
-    int system_id;
-    int autopilot_id;
-    int companion_id;
-
-    uint64_t write_count;
-
-    Mavlink_Messages current_messages;
-
-    functionQueue funQ;
-
-
-
-
+    inline functionQueue funQ;
+    // ------------------------------------------------
 
     void mavlink(Port *port_);
-
-    //~Mavlink();
-    //
-    //
-
     void handle_heartbeat(); 
-
     void handle_gps();
-
     void handle_attitude();
-
     void read_message();
-
     int write_message(mavlink_message_t message);
-
     int send_gps_message();
-
     int send_attitude_message();
-
     void send_both_messages();
-
     void start();
-
     void stop();
-
     void read_thread();
-
-    void start_read_thread();
-
-
 }
-
 
 #endif
--- include/obc_port.hpp
+++ include/obc_port.hpp
@@ -8,35 +8,18 @@
 #include <cstdlib>
 #include <deque>
 #include <functional>
-#include <iostream>
-
-
 #include <unistd.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
-#include <unistd.h>
-
-#include <iostream>
-#include <cstring>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <sys/socket.h>
 #include <sys/mman.h>
 #include <fcntl.h>
+#include <cstring>
 
+inline const size_t CHUNK_SIZE = 512;
+inline const char* SERVER_IP = "192.168.68.2"; 
+inline const int SERVER_PORT = 25565;
+inline const size_t SHM_SIZE = 1024 * 1024; 
 
-
-const size_t CHUNK_SIZE = 512;
-const char* SERVER_IP = "192.168.68.2";
-//const char* SERVER_IP = "10.241.90.10";
-const int SERVER_PORT = 25565;
-const size_t SHM_SIZE = 1024 * 1024;  // 1MB shared memory
-                                      //const size_t CHUNK_SIZE = 512;
-
-
-//TODO: remove the pragma or understand what it does
-//TODO: explain the magic number
-//TODO: to find if this is even nessicary
 #pragma pack(push, 1)
 struct Header {
     uint32_t magic = 0x12345678;
@@ -45,34 +28,13 @@ struct Header {
 };
 #pragma pack(pop)
 
-
 namespace OBCPort {
-
-    //TODO: make other instances of this bool
-    //bool camera_thread_started;
-    //int started = 0;
-    //TODO: change other instances of this to bool
-    //bool quit_signal;
-
     void quit();
-
-    //static int sockfd;
-
-    //TODO: don't make this shit  when transfering over to
-    // the cpp file
-    // TODO: remove???
     extern struct sockaddr_in client_addr;
-    //static socklen_t addrlen;
-
     bool send_image(void* img_addr, const size_t map_size);
-
     void start_camera_thread();
-
     void start_listener();
-
     void handle_command(char cmd);
-
-
 };
 
 #endif //__OBC_PORT_H__
